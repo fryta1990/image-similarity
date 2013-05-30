889cb3667d61d1516760d35b43299442428ce012
Grzegorz Polek <grzegorz.polek@gmail.com>
2013-05-31 00:17:43 +0200

    add ImageHolder (simple class for program's results) and JPEGFilter

---
+++ src/com/is/utils/ImageHolder.java
@@ -0,0 +1,34 @@
+package com.is.utils;
+
+import java.awt.image.RenderedImage;
+import java.io.File;
+
+public class ImageHolder {
+
+	File file = null;
+	RenderedImage render = null;
+	double distance = 11041; // 0 - 11041
+	
+	public ImageHolder(File file, RenderedImage render, double distance)
+	{
+		this.file = file;
+		this.render = render;
+		this.distance = distance;
+	}
+	
+	public File getFile()
+	{
+		return this.file;
+	}
+	
+	public RenderedImage getRender()
+	{
+		return this.render;
+	}
+	
+	public double getDistance()
+	{
+		return this.distance;
+	}
+	
+}
---
+++ src/coms/is/filters/JPEGFilter.java
@@ -0,0 +1,25 @@
+package coms.is.filters;
+
+import java.io.File;
+ 
+import javax.swing.filechooser.FileFilter;
+
+/*
+ * This class implements a generic file name filter that allows the listing/selection
+ * of JPEG files.
+ */
+public class JPEGFilter extends FileFilter implements java.io.FileFilter
+{
+	public boolean accept(File f)
+	{
+		if (f.getName().toLowerCase().endsWith(".jpeg")) return true;
+		if (f.getName().toLowerCase().endsWith(".jpg")) return true;
+		return false;
+	}
+	
+	public String getDescription()
+	{
+		return "JPEG files";
+	}
+ 
+}
